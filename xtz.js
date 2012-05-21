4ef8480f26a8e89cb1fdcfd14601700207e862a7
Oliver Caldwell <olliec87@gmail.com>
2012-05-21 10:22:32 +0100

    Made the default date interface which uses moment.

--- xtz.js
+++ xtz.js
@@ -6,11 +6,27 @@
      * This abstraction allows you to use any date library
      *
      * @param {Function} parse Called when parsing a date, is passed the date string
-     * @param {Function} format Called when displaying the date, is passed a formatting string
+     * @param {Function} format Called when displaying the date, is passed the returned value from parse and a formatting string
      */
     function DateInterface(parse, format) {
         // Store the methods
         this.parse = parse;
         this.format = format;
+    }
+    
+    // If moment is loaded then create an interface for it
+    // This will be used as the default date interface
+    // http://momentjs.com
+    if(typeof moment === 'function') {
+        var momentDI = new DateInterface(
+            function(date) {
+                // Parsing
+                return moment(date);
+            },
+            function(date, format) {
+                // Formating
+                return date.format(format);
+            }
+        );
     }
 }(this));
