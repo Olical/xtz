3423b39ec28656ce713d159da7b79824e0b58bc9
Oliver Caldwell <olliec87@gmail.com>
2012-05-21 10:48:33 +0100

    Began the date converted class. Currently just stores options.

--- xtz.js
+++ xtz.js
@@ -14,7 +14,7 @@
         this.format = format;
     }
     
-    // If moment is loaded then create an interface for it
+    // If moment.js is loaded then create an interface for it
     // This will be used as the default date interface
     // http://momentjs.com
     // Copy and edit this to create your own
@@ -32,11 +32,34 @@
         );
     }
     
+    /**
+     * Runs over all passed elements and converts them to a local time
+     * Will not run until you explicitly call `run` on it
+     * So you can call it when your library says the DOM is ready
+     *
+     * @param {Array|Element} els An element or array of elements that should be converted
+     * @param {DateInterface} di An optional interface for your date library, defaults to moment.js
+     */
+    function DateConverter(els, di) {
+        // Store the passed element(s)
+        this.els = els;
+        
+        // If there is a date interface then store it
+        // If not, use the default moment.js one
+        if(typeof di !== 'undefined') {
+            this.di = di;
+        }
+        else {
+            this.di = dateInterfaces.moment;
+        }
+    }
+    
     // Build the namespace to export and export it
     // If AMD is available then fire that too
     var namespace = {
         DateInterface: DateInterface,
-        dateInterfaces: dateInterfaces
+        dateInterfaces: dateInterfaces,
+        DateConverter: DateConverter
     };
     
     exports.xtz = namespace;
